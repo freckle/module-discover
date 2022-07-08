664c71dae376e66e254a0982d5cbf29f6429d8ee
Evan Rutledge Borden <eborden@frontrowed.com>
2022-07-08 13:00:24 -0500

    Initial implementation
    This version provides a function that can handle pre-processor
    arguments, finds modules based on a provided file pattern, and delivers
    those modules with metadata to a function to construct the output. This
    allows buiding of simple discovery pre-processors.

---
+++ example/Main.hs
@@ -0,0 +1,26 @@
+module Main
+  ( main
+  ) where
+
+import Prelude
+
+import Module.Discover.Run (Module(..), run)
+import System.Environment (getArgs)
+
+main :: IO ()
+main = getArgs >>= run (pure . make)
+
+make :: [Module] -> String
+make modules =
+  unlines
+    $ ["module Main (main) where", ""]
+    <> (toQualifiedImport <$> modules)
+    <> ["", "main :: IO ()", "main = do"]
+    <> (toMainCall <$> modules)
+
+toQualifiedImport :: Module -> String
+toQualifiedImport Module {..} =
+  "import qualified " <> moduleFullName <> " as " <> moduleName
+
+toMainCall :: Module -> String
+toMainCall Module {..} = "  " <> moduleName <> ".main"
--- library/Module/Discover/Run.hs
+++ library/Module/Discover/Run.hs
@@ -1,8 +1,65 @@
+
+
 module Module.Discover.Run
   ( run
+  , Module(..)
   ) where
 
 import Prelude
 
-run :: [String] -> IO ()
-run _ = pure ()
+import Data.Char (isUpper)
+import Data.List.Extra (replace)
+import System.Exit (exitFailure)
+import System.FilePath
+  ( dropExtension
+  , joinPath
+  , pathSeparator
+  , splitPath
+  , takeDirectory
+  , takeFileName
+  , (</>)
+  )
+import System.FilePattern.Directory (getDirectoryFiles)
+import System.IO (hPutStrLn, stderr)
+
+run
+  :: ([Module] -> IO String) -- ^ Output function
+  -> [String] -- ^ Processor arguments
+  -> IO ()
+run output args = do
+  case args of
+    src : _ : dst : filePattern : _args -> do
+      let srcDir = takeDirectory src
+      modules <- fmap (toModule srcDir)
+        <$> getDirectoryFiles srcDir [filePattern]
+      out <- output modules
+      writeFile dst out
+    _ -> do
+      hPutStrLn stderr "unexpected arguments"
+      exitFailure
+
+toModule :: FilePath -> FilePath -> Module
+toModule srcDir filePath = Module
+  { moduleName = dropExtension $ takeFileName filePath
+  , moduleFullName = toModuleFullName srcDir filePath
+  , moduleFilePath = srcDir </> filePath
+  }
+
+toModuleFullName :: FilePath -> FilePath -> String
+toModuleFullName srcDir filePath =
+  replace [pathSeparator] "."
+    $ dropExtension
+    $ joinPath
+    $ reverse
+    $ takeWhile (isUpper . head)
+    $ reverse
+    $ splitPath
+    $ srcDir
+    </> filePath
+
+data Module = Module
+  { moduleName :: String
+  , moduleFullName :: String
+  , moduleFilePath :: FilePath
+  }
+  deriving stock (Eq, Ord, Show)
